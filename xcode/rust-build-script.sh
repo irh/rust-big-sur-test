e4fdc1fa73c32ad9b54cce077367fe4f45e3f207
irh <ian.r.hobson@gmail.com>
2021-01-08 17:56:48 +0100

    Use cargo build directly

--- xcode/rust-build-script.sh
+++ xcode/rust-build-script.sh
@@ -1,8 +1,15 @@
-SDKROOT=`xcrun --show-sdk-path`
 # Commenting out the following line causes the build to fail on Big Sur
-export LIBRARY_PATH="$SDKROOT/usr/lib"
+export SDKROOT=`xcrun --sdk macosx --show-sdk-path`
+
+if [ "${PLATFORM_NAME}" = "iphoneos" ]; then
+  echo "Building for iOS devices: aarch64-apple-ios"
+  export CARGO_BUILD_TARGET="aarch64-apple-ios"
+else
+  echo "Unexpected platform"
+  exit 1
+fi
 
 export PATH="$HOME/.cargo/bin:$PATH"
 cd ../
 
-cargo lipo --xcode-integ --package testing_api
+cargo build
